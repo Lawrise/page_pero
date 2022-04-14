658623943c0747d19d4149016fa6b2bfe61e3520
Jembe <jembe20boisne@gmail.com>
2022-04-14 22:09:04 +0200

    fond qui change

---
+++ assets/vendor/php-email-form/php-email-form.php
@@ -0,0 +1,9 @@
+<?php
+    mail(
+        string $to,
+        string $subject,
+        string $message,
+        array|string $additional_headers = [],
+        string $additional_params = ""
+    ): bool
+?>
